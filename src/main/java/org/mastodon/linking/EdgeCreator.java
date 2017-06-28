33fc77b1386a499251808ca17567dbd574c4e7a0
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2017-06-28 13:53:11 +0200

    Change the EdgeCreator interface.
    - Accept Graph and edge Ref instances.
    - Accept linking cost.
    To make it more like to the VertexCreator interface.

--- src/main/java/org/mastodon/linking/EdgeCreator.java
+++ src/main/java/org/mastodon/linking/EdgeCreator.java
@@ -1,9 +1,10 @@
 package org.mastodon.linking;
 
 import org.mastodon.graph.Edge;
+import org.mastodon.graph.Graph;
 import org.mastodon.graph.Vertex;
 
 public interface EdgeCreator< V extends Vertex< E >, E extends Edge< V > >
 {
-	public E createEdge( V source, V target );
+	public E createEdge( Graph< V, E >graph, E ref, V source, V target, double edgeCost );
 }
