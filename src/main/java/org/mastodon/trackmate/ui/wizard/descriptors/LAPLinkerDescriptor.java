736e23272cc06c418a0e8b091f3c9de0804e2c1b
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2017-08-07 18:15:56 +0200

    Remove main() method from LAPLinkerDescriptor.

--- src/main/java/org/mastodon/trackmate/ui/wizard/descriptors/LAPLinkerDescriptor.java
+++ src/main/java/org/mastodon/trackmate/ui/wizard/descriptors/LAPLinkerDescriptor.java
@@ -2,34 +2,26 @@ package org.mastodon.trackmate.ui.wizard.descriptors;
 
 import static org.mastodon.detection.DetectorKeys.KEY_MAX_TIMEPOINT;
 import static org.mastodon.detection.DetectorKeys.KEY_MIN_TIMEPOINT;
-import static org.mastodon.linking.LinkerKeys.KEY_LINKING_FEATURE_PENALTIES;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Locale;
 import java.util.Map;
 
-import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
 
 import org.mastodon.detection.DetectorKeys;
 import org.mastodon.linking.LinkingUtils;
-import org.mastodon.linking.ProgressListeners;
 import org.mastodon.linking.mamut.SparseLAPLinkerMamut;
 import org.mastodon.linking.mamut.SpotLinkerOp;
 import org.mastodon.revised.mamut.WindowManager;
-import org.mastodon.revised.mamut.feature.DefaultMamutFeatureComputerService;
 import org.mastodon.revised.model.mamut.Model;
 import org.mastodon.trackmate.Settings;
 import org.mastodon.trackmate.TrackMate;
 import org.mastodon.trackmate.ui.wizard.WizardLogService;
-import org.scijava.Context;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 
@@ -77,7 +69,6 @@ public class LAPLinkerDescriptor extends SpotLinkerDescriptor
 		ls.put( KEY_MAX_TIMEPOINT, ds.get( KEY_MAX_TIMEPOINT ) );
 
 		settings.linkerSettings( ls );
-
 
 		final Integer setupID = ( Integer ) settings.values.getDetectorSettings().get( DetectorKeys.KEY_SETUP_ID );
 		final String units = ( null != setupID && null != settings.values.getSpimData() )
@@ -145,41 +136,5 @@ public class LAPLinkerDescriptor extends SpotLinkerDescriptor
 			this.configPanel = new LAPLinkerConfigPanel( "LAP linker.", units, model.getGraphFeatureModel() );
 			jScrollPaneMain.setViewportView( configPanel );
 		}
-	}
-
-	public static void main( final String[] args ) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException
-	{
-		UIManager.setLookAndFeel( UIManager.getSystemLookAndFeelClassName() );
-		Locale.setDefault( Locale.ROOT );
-		System.setProperty( "apple.laf.useScreenMenuBar", "true" );
-
-		final Context context = new org.scijava.Context();
-
-		final Model model = new Model();
-		final Settings settings = new Settings();
-		@SuppressWarnings( "unchecked" )
-		final Map< String, Double > linkingPenalties = ( Map< String, Double > ) settings.values.getLinkerSettings().get( KEY_LINKING_FEATURE_PENALTIES );
-		linkingPenalties.put( "Spot N links", 36.9 );
-
-		final TrackMate trackmate = new TrackMate( settings, model );
-		context.inject( trackmate );
-
-		final DefaultMamutFeatureComputerService featureComputerService = new DefaultMamutFeatureComputerService();
-		context.inject( featureComputerService );
-		featureComputerService.initialize();
-
-		System.out.println( "Found the following computers: " + featureComputerService.getAvailableVertexFeatureComputers() );
-		featureComputerService.compute( model, featureComputerService.getAvailableVertexFeatureComputers(), ProgressListeners.defaultLogger() );
-
-		final JFrame frame = new JFrame( "LAP config panel test" );
-		frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-		frame.setSize( 300, 600 );
-		final LAPLinkerDescriptor descriptor = new LAPLinkerDescriptor();
-		context.inject( descriptor );
-		descriptor.setTrackMate( trackmate );
-		frame.getContentPane().add( descriptor.targetPanel );
-		descriptor.aboutToDisplayPanel();
-		frame.setVisible( true );
-		descriptor.displayingPanel();
 	}
 }
