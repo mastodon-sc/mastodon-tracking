d9dd6497b35b19cc7d7ec5686bd97f4dde78145a
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2018-03-31 23:59:52 +0200

    Dummy Spot detector, to be used with advanced configuration.

---
+++ src/main/java/org/mastodon/detection/mamut/AdvancedDoGDetectorMamut.java
@@ -0,0 +1,17 @@
+package org.mastodon.detection.mamut;
+
+import org.scijava.Priority;
+import org.scijava.plugin.Plugin;
+
+/**
+ * Difference of Gaussian detector with advanced configuration.
+ *
+ * @author Tobias Pietzsch
+ * @author Jean-Yves Tinevez
+ */
+@Plugin( type = SpotDetectorOp.class, priority = Priority.NORMAL, name = "Advanced DoG detector",
+		description = "<html>"
+				+ "This detector is identifical to the DoG detector, but offers "
+				+ "more configuration options.</html>" )
+public class AdvancedDoGDetectorMamut extends DoGDetectorMamut
+{}
