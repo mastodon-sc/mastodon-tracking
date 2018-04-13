777c3c1468cbcf52ebf2638daa385ea987fec1f7
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2018-04-13 13:29:42 +0200

    The semi-auto tracker plugin has a command to configure the tracker.
    Its second command toggles the visibility of the config page, and
    changes made on this page are sent to the tracker.

--- src/main/java/org/mastodon/trackmate/semiauto/SemiAutomaticTrackerPlugin.java
+++ src/main/java/org/mastodon/trackmate/semiauto/SemiAutomaticTrackerPlugin.java
@@ -1,13 +1,21 @@
 package org.mastodon.trackmate.semiauto;
 
+import java.awt.BorderLayout;
+import java.awt.Frame;
 import java.awt.event.ActionEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.swing.JDialog;
+import javax.swing.WindowConstants;
+
 import org.mastodon.app.ui.ViewMenuBuilder.MenuItem;
+import org.mastodon.app.ui.settings.SettingsPanel;
 import org.mastodon.model.SelectionModel;
 import org.mastodon.plugin.MastodonPlugin;
 import org.mastodon.plugin.MastodonPluginAppModel;
@@ -15,6 +23,9 @@ import org.mastodon.revised.mamut.MamutMenuBuilder;
 import org.mastodon.revised.model.mamut.Link;
 import org.mastodon.revised.model.mamut.Model;
 import org.mastodon.revised.model.mamut.Spot;
+import org.mastodon.trackmate.semiauto.ui.SemiAutomaticTrackerConfigPage;
+import org.mastodon.trackmate.semiauto.ui.SemiAutomaticTrackerSettings;
+import org.mastodon.trackmate.semiauto.ui.SemiAutomaticTrackerSettingsManager;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 import org.scijava.ui.behaviour.util.AbstractNamedAction;
@@ -29,8 +40,10 @@ public class SemiAutomaticTrackerPlugin implements MastodonPlugin
 {
 
 	private static final String ACTION_1 = "[semiautotrack] track";
+	private static final String ACTION_2 = "[semiautotrack] show config";
 
 	private static final String[] ACTION_1_KEYS = new String[] { "ctrl T" };
+	private static final String[] ACTION_2_KEYS = new String[] { "not mapped" };
 
 	@Parameter
 	private OpService ops;
@@ -39,12 +52,21 @@ public class SemiAutomaticTrackerPlugin implements MastodonPlugin
 
 	private static Map< String, String > menuTexts = new HashMap<>();
 
+	private final Map< String, Object > currentSettings;
+
+	public SemiAutomaticTrackerPlugin()
+	{
+		this.currentSettings = new HashMap<>();
+		currentSettings.putAll( SemiAutomaticTrackerKeys.getDefaultDetectorSettingsMap() );
+	}
+
 	static
 	{
 		menuTexts.put( ACTION_1, "Semi-automatic tracking" );
+		menuTexts.put( ACTION_2, "Configure semi-automatic tracker" );
 	}
 
-	private final AbstractNamedAction action1 = new AbstractNamedAction( ACTION_1 )
+	private final AbstractNamedAction performSemiAutoTrackAction = new AbstractNamedAction( ACTION_1 )
 	{
 		private static final long serialVersionUID = 1L;
 
@@ -59,9 +81,11 @@ public class SemiAutomaticTrackerPlugin implements MastodonPlugin
 			if ( spots == null || spots.isEmpty() )
 				return;
 
-			final Map< String, Object > settings = SemiAutomaticTrackerKeys.getDefaultDetectorSettingsMap();
+			final Map< String, Object > settings = ( currentSettings == null )
+					? SemiAutomaticTrackerKeys.getDefaultDetectorSettingsMap()
+							: currentSettings;
 			final Model model = appModel.getAppModel().getModel();
-			
+
 			final SpimDataMinimal spimData = ( SpimDataMinimal ) appModel.getAppModel().getSharedBdvData().getSpimData();
 			final SemiAutomaticTracker tracker = ( SemiAutomaticTracker ) Computers.binary(
 					ops, SemiAutomaticTracker.class, model, spots, settings,
@@ -69,6 +93,22 @@ public class SemiAutomaticTrackerPlugin implements MastodonPlugin
 			tracker.compute( spots, settings, model );
 		}
 	};
+
+	private final AbstractNamedAction toggleConfigDialogVisibility = new AbstractNamedAction( ACTION_2)
+	{
+
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		public void actionPerformed( final ActionEvent e )
+		{
+			if (null == dialog)
+				return;
+			dialog.setVisible( !dialog.isVisible() );
+		}
+	};
+
+	private JDialog dialog;
 
 	@Override
 	public Map< String, String > getMenuTexts()
@@ -82,19 +122,54 @@ public class SemiAutomaticTrackerPlugin implements MastodonPlugin
 		return Arrays.asList(
 				MamutMenuBuilder.menu( "Plugins",
 						MamutMenuBuilder.menu( "TrackMate",
-								MamutMenuBuilder.item( ACTION_1 ) ) ) );
+								MamutMenuBuilder.item( ACTION_1 ) ) ),
+				MamutMenuBuilder.menu( "Plugins",
+						MamutMenuBuilder.menu( "TrackMate",
+								MamutMenuBuilder.item( ACTION_2 ) ) ));
 	}
 
 	@Override
 	public void installGlobalActions( final Actions actions )
 	{
-		actions.namedAction( action1, ACTION_1_KEYS );
+		actions.namedAction( performSemiAutoTrackAction, ACTION_1_KEYS );
+		actions.namedAction( toggleConfigDialogVisibility, ACTION_2_KEYS );
 	}
 
 	@Override
 	public void setAppModel( final MastodonPluginAppModel appModel )
 	{
 		this.appModel = appModel;
-	}
 
+		final SpimDataMinimal spimData = ( SpimDataMinimal ) appModel.getAppModel().getSharedBdvData().getSpimData();
+		final SemiAutomaticTrackerSettingsManager styleManager = new SemiAutomaticTrackerSettingsManager();
+		final SemiAutomaticTrackerConfigPage page = new SemiAutomaticTrackerConfigPage( "Settings", styleManager, spimData )
+		{
+			@Override
+			public void apply()
+			{
+				super.apply();
+				final SemiAutomaticTrackerSettings forwardDefaultStyle = styleManager.getForwardDefaultStyle();
+				currentSettings.clear();
+				currentSettings.putAll( forwardDefaultStyle.getAsSettingsMap() );
+			}
+		};
+		final SettingsPanel settings = new SettingsPanel();
+		settings.addPage( page );
+
+		dialog = new JDialog( ( Frame ) null, "Semi-automatic tracker settings" );
+		dialog.getContentPane().add( settings, BorderLayout.CENTER );
+		settings.onOk( () -> dialog.setVisible( false )  );
+		settings.onCancel( () -> dialog.setVisible( false ) );
+
+		dialog.setDefaultCloseOperation( WindowConstants.DO_NOTHING_ON_CLOSE );
+		dialog.addWindowListener( new WindowAdapter()
+		{
+			@Override
+			public void windowClosing( final WindowEvent e )
+			{
+				settings.cancel();
+			}
+		} );
+		dialog.pack();
+	}
 }
