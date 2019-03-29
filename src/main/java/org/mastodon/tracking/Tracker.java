92d067770784b15d696cb46e257137fa544ca077
Jean-Yves TINEVEZ <jean-yves.tinevez@pasteur.fr>
2019-03-29 17:45:49 +0100

    Add a tracker hierarchy that implement different motion models.
    So far, one bor Brownian motion, one for near constant velocity with
    Kalman filter.

---
+++ src/main/java/org/mastodon/tracking/Tracker.java
@@ -0,0 +1,39 @@
+package org.mastodon.tracking;
+
+import net.imglib2.RealLocalizable;
+
+/**
+ * Interface for tracker that implements a specific motion model.
+ *
+ * @author Jean-Yves Tinevez
+ *
+ */
+public interface Tracker
+{
+
+	/**
+	 * Updates this tracker with the specified detection position.
+	 *
+	 * @param detection
+	 *            the new detection position.
+	 */
+	public void update( RealLocalizable detection );
+
+	/**
+	 * Returns the candidate predicted position for the next step in tracking.
+	 *
+	 * @return the predicted position.
+	 */
+	public RealLocalizable predict();
+
+	/**
+	 * Returns the cost set by this tracker to link the predicted position to
+	 * the specified position.
+	 *
+	 * @param position
+	 *            the position to compute to cost to link to.
+	 * @return the cost to link to the specified position.
+	 */
+	public double costTo( RealLocalizable position );
+
+}
