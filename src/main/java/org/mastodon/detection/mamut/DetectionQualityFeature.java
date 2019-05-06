bcb7780fb73d4bd5eeefbebcabe69eb752f25ed9
Jean-Yves TINEVEZ <jean-yves.tinevez@pasteur.fr>
2019-05-06 15:30:14 +0200

    Serializers for the DetectionQualityFeature and the LinkCostFeature.

--- src/main/java/org/mastodon/detection/mamut/DetectionQualityFeature.java
+++ src/main/java/org/mastodon/detection/mamut/DetectionQualityFeature.java
@@ -1,10 +1,22 @@
 package org.mastodon.detection.mamut;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
 import org.mastodon.RefPool;
+import org.mastodon.collection.RefCollection;
 import org.mastodon.feature.Dimension;
 import org.mastodon.feature.DoubleScalarFeature;
+import org.mastodon.feature.DoubleScalarFeatureSerializer;
 import org.mastodon.feature.FeatureModel;
+import org.mastodon.feature.FeatureProjectionSpec;
+import org.mastodon.feature.FeatureSpec;
+import org.mastodon.feature.Multiplicity;
+import org.mastodon.feature.io.FeatureSerializer;
+import org.mastodon.io.FileIdToObjectMap;
+import org.mastodon.properties.DoublePropertyMap;
 import org.mastodon.revised.model.mamut.Spot;
+import org.scijava.plugin.Plugin;
 
 public class DetectionQualityFeature extends DoubleScalarFeature< Spot >
 {
@@ -13,9 +25,37 @@ public class DetectionQualityFeature extends DoubleScalarFeature< Spot >
 
 	private static final String HELP_STRING = "Report the detection quality.";
 
+	public static final Spec SPEC = new Spec();
+
+	@Plugin(type = FeatureSpec.class)
+	public static class Spec extends FeatureSpec< DetectionQualityFeature, Spot >
+	{
+		public Spec()
+		{
+			super(
+					KEY,
+					HELP_STRING,
+					DetectionQualityFeature.class,
+					Spot.class,
+					Multiplicity.SINGLE,
+					new FeatureProjectionSpec( KEY, Dimension.QUALITY) );
+		}
+	}
+
 	public DetectionQualityFeature( final RefPool< Spot > pool )
 	{
-		super( KEY, HELP_STRING, Dimension.QUALITY, Dimension.QUALITY_UNITS, pool );
+		super( KEY, Dimension.QUALITY, Dimension.QUALITY_UNITS, pool );
+	}
+
+	private DetectionQualityFeature( final DoublePropertyMap< Spot > map )
+	{
+		super( KEY, Dimension.QUALITY, Dimension.QUALITY_UNITS, map );
+	}
+
+	@Override
+	public FeatureSpec< DetectionQualityFeature, Spot > getSpec()
+	{
+		return SPEC;
 	}
 
 	/**
@@ -40,5 +80,23 @@ public class DetectionQualityFeature extends DoubleScalarFeature< Spot >
 			return feature;
 		}
 		return retrieved;
+	}
+
+	@Plugin( type = FeatureSerializer.class )
+	public static class DetectionQualityFeatureSerializer extends DoubleScalarFeatureSerializer< DetectionQualityFeature, Spot >
+	{
+
+		@Override
+		public DetectionQualityFeature deserialize( final FileIdToObjectMap< Spot > idmap, final RefCollection< Spot > pool, final ObjectInputStream ois ) throws IOException, ClassNotFoundException
+		{
+			final DeserializedStruct struct = read( idmap, pool, ois );
+			return new DetectionQualityFeature( struct.map );
+		}
+
+		@Override
+		public FeatureSpec< DetectionQualityFeature, Spot > getFeatureSpec()
+		{
+			return SPEC;
+		}
 	}
 }
--- src/main/java/org/mastodon/linking/mamut/LinkCostFeature.java
+++ src/main/java/org/mastodon/linking/mamut/LinkCostFeature.java
@@ -1,10 +1,22 @@
 package org.mastodon.linking.mamut;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
 import org.mastodon.RefPool;
+import org.mastodon.collection.RefCollection;
 import org.mastodon.feature.Dimension;
 import org.mastodon.feature.DoubleScalarFeature;
+import org.mastodon.feature.DoubleScalarFeatureSerializer;
 import org.mastodon.feature.FeatureModel;
+import org.mastodon.feature.FeatureProjectionSpec;
+import org.mastodon.feature.FeatureSpec;
+import org.mastodon.feature.Multiplicity;
+import org.mastodon.feature.io.FeatureSerializer;
+import org.mastodon.io.FileIdToObjectMap;
+import org.mastodon.properties.DoublePropertyMap;
 import org.mastodon.revised.model.mamut.Link;
+import org.scijava.plugin.Plugin;
 
 public class LinkCostFeature extends DoubleScalarFeature< Link >
 {
@@ -12,9 +24,37 @@ public class LinkCostFeature extends DoubleScalarFeature< Link >
 
 	private static final String HELP_STRING = "Report the link cost.";
 
+	public static final Spec SPEC = new Spec();
+
+	@Plugin(type = FeatureSpec.class)
+	public static class Spec extends FeatureSpec< LinkCostFeature, Link >
+	{
+		public Spec()
+		{
+			super(
+					KEY,
+					HELP_STRING,
+					LinkCostFeature.class,
+					Link.class,
+					Multiplicity.SINGLE,
+					new FeatureProjectionSpec( KEY, Dimension.COST) );
+		}
+	}
+
 	public LinkCostFeature( final RefPool< Link > pool )
 	{
-		super( KEY, HELP_STRING, Dimension.COST, Dimension.COST_UNITS, pool );
+		super( KEY, Dimension.COST, Dimension.COST_UNITS, pool );
+	}
+
+	private LinkCostFeature( final DoublePropertyMap< Link > map )
+	{
+		super( KEY, Dimension.COST, Dimension.COST_UNITS, map );
+	}
+
+	@Override
+	public FeatureSpec< LinkCostFeature, Link > getSpec()
+	{
+		return SPEC;
 	}
 
 	/**
@@ -39,5 +79,24 @@ public class LinkCostFeature extends DoubleScalarFeature< Link >
 			return feature;
 		}
 		return retrieved;
+	}
+
+	@Plugin( type = FeatureSerializer.class )
+	public static class LinkCostFeatureFeatureSerializer extends DoubleScalarFeatureSerializer< LinkCostFeature, Link >
+	{
+
+
+		@Override
+		public LinkCostFeature deserialize( final FileIdToObjectMap< Link > idmap, final RefCollection< Link > pool, final ObjectInputStream ois ) throws IOException, ClassNotFoundException
+		{
+			final DeserializedStruct struct = read( idmap, pool, ois );
+			return new LinkCostFeature( struct.map );
+		}
+
+		@Override
+		public FeatureSpec< LinkCostFeature, Link > getFeatureSpec()
+		{
+			return SPEC;
+		}
 	}
 }
