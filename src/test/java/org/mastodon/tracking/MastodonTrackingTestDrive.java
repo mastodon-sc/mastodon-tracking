a7254925f982ce0402ee52b37952bcda5493c421
Jean-Yves TINEVEZ <jean-yves.tinevez@pasteur.fr>
2020-09-16 12:37:28 +0200

    Tweak the interactive test drive.

--- src/test/java/org/mastodon/tracking/MastodonTrackingTestDrive.java
+++ src/test/java/org/mastodon/tracking/MastodonTrackingTestDrive.java
@@ -1,6 +1,5 @@
 package org.mastodon.tracking;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.Locale;
 
@@ -10,6 +9,7 @@ import javax.swing.UnsupportedLookAndFeelException;
 import org.mastodon.mamut.MainWindow;
 import org.mastodon.mamut.WindowManager;
 import org.mastodon.mamut.project.MamutProject;
+import org.mastodon.mamut.project.MamutProjectIO;
 import org.scijava.Context;
 
 import mpicbg.spim.data.SpimDataException;
@@ -24,7 +24,8 @@ public class TestDrive
 		System.setProperty( "apple.laf.useScreenMenuBar", "true" );
 		final Context context = new Context();
 
-		final MamutProject project = new MamutProject( null, new File( "../mastodon/samples/datasethdf5.xml" ) );
+//		final MamutProject project = new MamutProject( null, new File( "../mastodon/samples/datasethdf5.xml" ) );
+		final MamutProject project = new MamutProjectIO().load( "../mastodon/samples/drosophila_crop.mastodon" );
 
 		final WindowManager windowManager = new WindowManager( context );
 		final MainWindow mw = new MainWindow( windowManager );
