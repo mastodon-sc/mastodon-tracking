be59778c100f7a37098174a0336ee8a7bd009fe9
Jean-Yves Tinevez <tinevez@pasteur.fr>
2023-12-01 14:11:43 +0100

    Update license blurbs.

--- src/main/java/org/mastodon/tracking/mamut/trackmate/wizard/DetectionSequence.java
+++ src/main/java/org/mastodon/tracking/mamut/trackmate/wizard/DetectionSequence.java
@@ -2,7 +2,7 @@
  * #%L
  * mastodon-tracking
  * %%
- * Copyright (C) 2017 - 2022 Tobias Pietzsch, Jean-Yves Tinevez
+ * Copyright (C) 2017 - 2023 Tobias Pietzsch, Jean-Yves Tinevez
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
